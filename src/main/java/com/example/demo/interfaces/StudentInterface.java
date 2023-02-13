e6a887dc262385b58df581392382e1668a26dd32
CodeTalks <info@codetalksdna.com>
2023-02-13 10:14:51 +0530

    Written Custom Query

--- src/main/java/com/example/demo/interfaces/StudentInterface.java
+++ src/main/java/com/example/demo/interfaces/StudentInterface.java
@@ -2,6 +2,7 @@ package com.example.demo.interfaces;
 
 import com.example.demo.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +11,10 @@ import java.util.Optional;
 @Repository
 public interface StudentInterface extends JpaRepository<StudentEntity, Integer> {
 
+    //TODO:- CREATE CUSTOM QUERY FOR FINDING SECTION,SCHOOL NAME.
     List<StudentEntity> findByName(String name);
     List<StudentEntity> findBySection(String section);
     List<StudentEntity> findBySchoolName(String schoolName);
+    @Query(value = "select * from Student_info s where s.name=:name" ,nativeQuery = true)
+    List<StudentEntity> getName(String name);
 }
