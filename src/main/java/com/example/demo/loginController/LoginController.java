0bf293a18b5241486d545748d0490c25e77d22d0
CodeTalks <info@codetalksdna.com>
2023-02-14 12:20:08 +0530

    Fixed Issue with authentication

---
+++ src/main/java/com/example/demo/loginController/LoginController.java
@@ -0,0 +1,23 @@
+package com.example.demo.loginController;
+
+import com.example.demo.entity.LoginEntity;
+import com.example.demo.response.LoginResponse;
+import com.example.demo.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping("/saveUserNamePassword")
+    public LoginResponse saveUserNamePassword(@RequestBody LoginEntity loginEntity){
+        return loginService.saveUserNamePassword(loginEntity);
+    }
+}
