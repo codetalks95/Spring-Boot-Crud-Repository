7cd593bbe060a6d391707444d5a4e601758d422d
CodeTalks <info@codetalksdna.com>
2023-02-21 11:58:34 +0530

    added changes

---
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -0,0 +1,19 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "StudentInfo")
+public class StudentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private int ID;
+    private String name;
+    private String section;
+    private String schoolName;
+}
---
+++ src/main/java/com/example/demo/response/StudentResponse.java
@@ -0,0 +1,55 @@
+package com.example.demo.response;
+
+import com.example.demo.entity.StudentEntity;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public class StudentResponse {
+
+    private String message;
+    private HttpStatus statusCode;
+    private StudentEntity studentEntity;
+    private List<StudentEntity> studentEntityList;
+    private String dateTime;
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public HttpStatus getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(HttpStatus statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public StudentEntity getStudentEntity() {
+        return studentEntity;
+    }
+
+    public void setStudentEntity(StudentEntity studentEntity) {
+        this.studentEntity = studentEntity;
+    }
+
+    public List<StudentEntity> getStudentEntityList() {
+        return studentEntityList;
+    }
+
+    public void setStudentEntityList(List<StudentEntity> studentEntityList) {
+        this.studentEntityList = studentEntityList;
+    }
+}
